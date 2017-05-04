c56bf2c78804292b1f69e4a3aa98e72ea9e257e0
Nitesh Isave <initesh@icloud.com>
2017-05-04 19:49:03 +0530

    Initial Commit

---
+++ GoogleContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleContacts
+//
+//  Created by Nitesh I on 04/05/17.
+//  Copyright © 2017 Nitesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
